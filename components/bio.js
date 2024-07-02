f25118ff9c0bc231baa44dad73685bb1f4c9ac39
Yves Gaetan Nana Teukam <56451020+yvesnana@users.noreply.github.com>
2024-07-02 22:45:22 +0200

    Update bio.js

--- components/bio.js
+++ components/bio.js
@@ -2,11 +2,9 @@ import { Box } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 
 export const BioSection = styled(Box)`
-  paddingLeft: 3.4em;
-  text-indent: -3.4em;
+  
 `
 
 export const BioYear = styled(Box)`
-  font-weight: bold;
-  margin-right: 1em;
+
 `
