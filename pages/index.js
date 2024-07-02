9caddcab3ae43d7ffc2368b92ce67eb24551fa22
Yves Gaetan Nana Teukam <56451020+yvesnana@users.noreply.github.com>
2024-07-02 21:27:53 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -71,15 +71,15 @@ const Home = () => (
         >
         Hi! I'm a data scientist based in Zürich. I have 4 years of experience in developing 
         AI/ML-based tools to address complex biological problems. I'm currently completing a Ph.D. in Biomedical Engineering 
-        with a focus on Language Modelling for Protein Design at <Link as={NextLink} href="https://research.ibm.com/labs/zurich/" target='blank_' passHref scroll={false}> IBM Research Zürich</Link>
-        and <Link as={NextLink} href="https://www.tue.nl/en/" target='blank_' passHref scroll={false}> Eindhoven University of Technology</Link>,
+        with a focus on Language Modelling for Protein Design at <Link as={NextLink} href="https://research.ibm.com/labs/zurich/" target='blank_' passHref scroll={false}> IBM Research Zürich </Link>
+        and <Link as={NextLink} href="https://www.tue.nl/en/" target='blank_' passHref scroll={false}> Eindhoven University of Technology </Link>,
         expecting to graduate in January 2025. My expertise includes machine learning, generative modelling, NLP, Omics, and bioinformatics. 
         I have a proven track record of optimizing biomolecules and enhancing model performance for biocatalysis, drug discovery, and green chemistry. 
         I'm proficient in advanced machine learning frameworks and deep learning libraries, 
-        such as <Link as={NextLink} href="https://www.tensorflow.org/" target='blank_' passHref scroll={false}> TensorFlow</Link>, <Link as={NextLink} href="https://huggingface.co/docs/transformers/index" target='blank_' passHref scroll={false}> Transformers</Link>,
-        and <Link as={NextLink} href="https://pytorch.org/" target='blank_' passHref scroll={false}> PyTorch</Link>,
-        for analyzing and modeling complex biological datasets. I've also contributed to open-source projects like <Link as={NextLink} href="https://github.com/GT4SD" target='blank_' passHref scroll={false}> GT4SD</Link>
-        and published in high-impact journals like  <Link as={NextLink} href="https://www.nature.com/comms/" target='blank_' passHref scroll={false}> Nature Communications</Link>. 
+        such as <Link as={NextLink} href="https://www.tensorflow.org/" target='blank_' passHref scroll={false}> TensorFlow </Link>, <Link as={NextLink} href="https://huggingface.co/docs/transformers/index" target='blank_' passHref scroll={false}> Transformers </Link>,
+        and <Link as={NextLink} href="https://pytorch.org/" target='blank_' passHref scroll={false}> PyTorch </Link>,
+        for analyzing and modeling complex biological datasets. I've also contributed to open-source projects like <Link as={NextLink} href="https://github.com/GT4SD" target='blank_' passHref scroll={false}> GT4SD </Link>
+        and published in high-impact journals like  <Link as={NextLink} href="https://www.nature.com/comms/" target='blank_' passHref scroll={false}> Nature Communications </Link>. 
         I am fluent in English, French, Italian, and Spanish. I am strongly committed to driving innovative projects, fostering collaboration, and achieving team goals.
         </Paragraph>
       </Section>
@@ -89,30 +89,20 @@ const Home = () => (
           Bio
         </Heading>
         <BioSection>
-          <BioYear>1994</BioYear>
-          Born and raised in Spain, Argentinian descendant.
+          <BioYear>01/2022 to 01/2025 (expected graduation)</BioYear>
+          Ph.D. in Biomedical Engineering at IBM Research Zürich & Eindhoven University of Technology – Zürich, Switzerland & Eindhoven, Netherlands
         </BioSection>
         <BioSection>
-          <BioYear>2016</BioYear>
-          B.Sc. Economics and Finance at Universidad Autónoma de Madrid.
+          <BioYear>09/2019 to 10/2021</BioYear>
+          Master of Science in Data Science at University of Rome La Sapienza – Roma, Italy
         </BioSection>
         <BioSection>
-          <BioYear>2017</BioYear>
-          M.Sc. Data Science at CUNEF and worked at {''} 
-          <Link href="https://www.etsfactory.com/" target="_blank">
-              ETS Asset Management.
-          </Link>
+          <BioYear>09/2018 to 02/2019</BioYear>
+          Exchange Program Erasmus at ESCI-Universitad Pompeu Fabra – Barcelona, Spain
         </BioSection>
         <BioSection>
-          <BioYear>2018</BioYear>
-          Worked at {''} 
-          <Link href="https://keepler.io/" target="_blank">
-              Keepler Data Tech.
-          </Link>
-        </BioSection>
-        <BioSection>
-          <BioYear>2020 to present</BioYear>
-          Working at the Bank of Spain.
+          <BioYear>09/2016 to 06/2019</BioYear>
+          Bachelor of Science in Bioinformatics at University of Rome La Sapienza – Roma, Italy
         </BioSection>
       </Section>
 
@@ -122,7 +112,7 @@ const Home = () => (
         </Heading>
         <BioSection>
           <BioYear>Email</BioYear>
-          nicolas.forteza@bde.es
+          yves.g.nana@gmail.com
         </BioSection>
       </Section>
 
@@ -132,19 +122,7 @@ const Home = () => (
         </Heading>
         <List spacing={0}>
           <ListItem>
-            <Link href="https://github.com/nicoforteza" target="_blank">
-              <Button
-                variant="ghost"
-                style={{"textAlign": "left !important", "paddingLeft": "0px"}}
-                colorScheme={mode('#319795', '#ff63c3')}
-                leftIcon={<IoLogoGithub />}
-              >
-                Github
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://twitter.com/nicoforteza1" target="_blank">
+            <Link href="https://twitter.com/YvesGNT" target="_blank">
               <Button
                 variant="ghost"
                 style={{"textAlign": "left !important", "paddingLeft": "0px"}}
@@ -157,7 +135,7 @@ const Home = () => (
             </Link>
           </ListItem>
           <ListItem>
-            <Link href="https://www.linkedin.com/in/nicoforteza/" target="_blank">
+            <Link href="https://www.linkedin.com/in/nana-yves/" target="_blank">
               <Button
                 variant="ghost"
                 style={{"textAlign": "left !important", "paddingLeft": "0px"}}
