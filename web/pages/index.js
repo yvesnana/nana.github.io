e984207780b89d6e7ce053ff354f2adbca47541b
nanayves <yves.g.nana@gmail.com>
2024-07-02 15:57:55 +0200

    all

--- web/pages/index.js
+++ web/pages/index.js
@@ -4,35 +4,43 @@ import {
   Container,
   Heading,
   Box,
+  SimpleGrid,
   Button,
   List,
   ListItem,
-  chakra
+  useColorModeValue
 } from '@chakra-ui/react'
+import { ChevronRightIcon, EmailIcon } from '@chakra-ui/icons'
 import Paragraph from '../components/paragraph'
 import { BioSection, BioYear } from '../components/bio'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
-import { IoLogoTwitter, IoLogoGithub, IoLogoLinkedin, IoFileTraySharp } from 'react-icons/io5'
+import { GridItem } from '../components/grid-item'
+import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
+import thumbYouTube from '../public/images/links/youtube.png'
+import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
 import Image from 'next/image'
-import { mode } from '@chakra-ui/theme-tools'
-import me from '../public/me.jpg'
-
 
 const Home = () => (
   <Layout>
-    <Container maxW="container.md">
-      
+    <Container>
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        css={{ backdropFilter: 'blur(10px)' }}
+      >
+        Hello, I&apos;m an indie app developer based in Japan!
+      </Box>
+
       <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
-          <Heading 
-          as="h2" 
-          variant="page-title"
-          style={{'letterSpacing': '-0.3px'}}
-          >
-            <h1>Yves Gaetan Nana Teukam</h1>
+          <Heading as="h2" variant="page-title">
+            Takuya Matsuyama
           </Heading>
-          <p>Data Scientist</p>
+          <p>Digital Craftsman ( Artist / Developer / Designer )</p>
         </Box>
         <Box
           flexShrink={0}
@@ -42,18 +50,17 @@ const Home = () => (
         >
           <Box
             borderColor="whiteAlpha.800"
-            borderWidth={0}
+            borderWidth={2}
             borderStyle="solid"
             w="100px"
             h="100px"
             display="inline-block"
-            borderradius="full"
+            borderRadius="full"
             overflow="hidden"
           >
             <Image
-              src={me}
+              src="/images/takuya.jpg"
               alt="Profile image"
-              borderradius="full"
               width="100"
               height="100"
             />
@@ -62,39 +69,42 @@ const Home = () => (
       </Box>
 
       <Section delay={0.1}>
-
-      <Heading as="h3" variant="section-title">
-          Forthcoming Conferences:
-        </Heading>
-
-        <Paragraph>
-        <Link as={NextLink} href="https://ww2.amstat.org/meetings/jsm/2024/" target='blank_' passHref scroll={false}>
-            Joint Statistical Meeting {}
-          </Link>
-           (JSM) of the American Statistical Association (Portland, August 2024)
-        </Paragraph>
-
         <Heading as="h3" variant="section-title">
-          About me
+          Work
         </Heading>
-        <Paragraph
-        
-        >
-        Hi! I'm a data scientist based in Madrid who enjoys creating data products and 
-        research in economics. I currently work at the DG of Economics, Statistics 
-        and Research of the {' '}
-          <Link as={NextLink} href="https://www.bde.es/bde/es/" target='blank_' passHref scroll={false}>
-            Bank of Spain
+        <Paragraph>
+          Takuya is a freelance and a full-stack developer based in Osaka with a
+          passion for building digital services/stuff he wants. He has a knack
+          for all things launching products, from planning and designing all the
+          way to solving real-life problems with code. When not online, he loves
+          hanging out with his camera. Currently, he is living off of his own
+          product called{' '}
+          <Link as={NextLink} href="/works/inkdrop" passHref scroll={false}>
+            Inkdrop
           </Link>
-          . I'm part of the team that conducts the {' '}
-          <Link as={NextLink} href="https://app.bde.es/efs_www/home?lang=ES" target='blank_' passHref scroll={false}>
-            Spanish Survey of Household Finances
+          . He publishes content for marketing his products and his YouTube
+          channel called &quot;
+          <Link
+            as={NextLink}
+            href="https://www.youtube.com/devaslife"
+            passHref
+            target="_blank"
+          >
+            Dev as Life
           </Link>
-          . In addition, I contribute to research on multiple Economics topics using data science techniques. I invite you to take a look at <span></span>
-          <Link as={NextLink} href="/works" passHref scroll={false}>
-            my work.
-          </Link>
+          &quot; has more than 100k subscribers.
         </Paragraph>
+        <Box align="center" my={4}>
+          <Button
+            as={NextLink}
+            href="/works"
+            scroll={false}
+            rightIcon={<ChevronRightIcon />}
+            colorScheme="teal"
+          >
+            My portfolio
+          </Button>
+        </Box>
       </Section>
 
       <Section delay={0.2}>
@@ -102,100 +112,133 @@ const Home = () => (
           Bio
         </Heading>
         <BioSection>
-          <BioYear>1994</BioYear>
-          Born and raised in Spain, Argentinian descendant.
+          <BioYear>1984</BioYear>
+          Born in Osaka (大阪), Japan.
         </BioSection>
         <BioSection>
-          <BioYear>2016</BioYear>
-          B.Sc. Economics and Finance at Universidad Autónoma de Madrid.
+          <BioYear>2010</BioYear>
+          Completed the Master&apos;s Program in the Graduate School of
+          Information Science at Nara Institute of Science and Technology
+          (奈良先端科学技術大学院大学情報科学研究科修士課程)
         </BioSection>
         <BioSection>
-          <BioYear>2017</BioYear>
-          M.Sc. Data Science at CUNEF and worked at {''} 
-          <Link href="https://www.etsfactory.com/" target="_blank">
-              ETS Asset Management.
-          </Link>
+          <BioYear>2010</BioYear>
+          Worked at Yahoo! Japan (ヤフー株式会社入社)
         </BioSection>
         <BioSection>
-          <BioYear>2018</BioYear>
-          Worked at {''} 
-          <Link href="https://keepler.io/" target="_blank">
-              Keepler Data Tech.
-          </Link>
-        </BioSection>
-        <BioSection>
-          <BioYear>2020 to present</BioYear>
-          Working at the Bank of Spain.
+          <BioYear>2012 to present</BioYear>
+          Working as a freelancer
         </BioSection>
       </Section>
 
-      <Section delay={0.2}>
+      <Section delay={0.3}>
         <Heading as="h3" variant="section-title">
-          Contact
+          I ♥
         </Heading>
-        <BioSection>
-          <BioYear>Email</BioYear>
-          nicolas.forteza@bde.es
-        </BioSection>
+        <Paragraph>
+          Art, Music,{' '}
+          <Link href="https://illust.odoruinu.net/" target="_blank">
+            Drawing
+          </Link>
+          , Playing Drums,{' '}
+          <Link href="https://500px.com/p/craftzdog" target="_blank">
+            Photography
+          </Link>
+          , Leica, Machine Learning
+        </Paragraph>
       </Section>
 
       <Section delay={0.3}>
         <Heading as="h3" variant="section-title">
           On the web
         </Heading>
-        <List spacing={0}>
+        <List>
           <ListItem>
-            <Link href="https://github.com/nicoforteza" target="_blank">
+            <Link href="https://github.com/craftzdog" target="_blank">
               <Button
                 variant="ghost"
-                style={{"textAlign": "left !important", "paddingLeft": "0px"}}
-                colorScheme={mode('#319795', '#ff63c3')}
+                colorScheme="teal"
                 leftIcon={<IoLogoGithub />}
               >
-                Github
+                @craftzdog
               </Button>
             </Link>
           </ListItem>
           <ListItem>
-            <Link href="https://twitter.com/nicoforteza1" target="_blank">
+            <Link href="https://twitter.com/inkdrop_app" target="_blank">
               <Button
                 variant="ghost"
-                style={{"textAlign": "left !important", "paddingLeft": "0px"}}
-                colorScheme={mode('#319795', '#ff63c3')}
+                colorScheme="teal"
                 leftIcon={<IoLogoTwitter />}
-                paddingLeft={0}
               >
-                X/Twitter
+                @inkdrop_app (English)
               </Button>
             </Link>
           </ListItem>
           <ListItem>
-            <Link href="https://www.linkedin.com/in/nicoforteza/" target="_blank">
+            <Link href="https://twitter.com/craftzdog" target="_blank">
               <Button
                 variant="ghost"
-                style={{"textAlign": "left !important", "paddingLeft": "0px"}}
-                colorScheme={mode('#319795', '#ff63c3')}
-                leftIcon={<IoLogoLinkedin />}
-              >  Linkedin
+                colorScheme="teal"
+                leftIcon={<IoLogoTwitter />}
+              >
+                @craftzdog (日本語)
               </Button>
             </Link>
           </ListItem>
           <ListItem>
-            <Link href="https://orcid.org/0009-0003-7227-9338" target="_blank">
+            <Link href="https://instagram.com/craftzdog" target="_blank">
               <Button
                 variant="ghost"
-                style={{"textAlign": "left !important", "paddingLeft": "0px"}}
-                colorScheme={mode('#319795', '#ff63c3')}
-                leftIcon={<IoFileTraySharp />}
-              >  Orcid
+                colorScheme="teal"
+                leftIcon={<IoLogoInstagram />}
+              >
+                @craftzdog
               </Button>
             </Link>
           </ListItem>
         </List>
+
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            href="https://www.youtube.com/devaslife"
+            title="Dev as Life"
+            thumbnail={thumbYouTube}
+          >
+            My YouTube channel (&gt;150k subs)
+          </GridItem>
+          <GridItem
+            href="https://www.inkdrop.app/"
+            title="Inkdrop"
+            thumbnail={thumbInkdrop}
+          >
+            A Markdown note-taking app
+          </GridItem>
+        </SimpleGrid>
+
+        <Heading as="h3" variant="section-title">
+          Newsletter
+        </Heading>
+        <p>
+          Join me on a behind-the-scenes coding journey. Weekly updates on
+          projects, tutorials, and videos
+        </p>
+
+        <Box align="center" my={4}>
+          <Button
+            as={NextLink}
+            href="https://www.devas.life/"
+            scroll={false}
+            leftIcon={<EmailIcon />}
+            colorScheme="teal"
+          >
+            Sign up my newsletter here
+          </Button>
+        </Box>
       </Section>
     </Container>
   </Layout>
 )
 
 export default Home
-// export { getStaticProps } from '../components/chakra'
+export { getServerSideProps } from '../components/chakra'
