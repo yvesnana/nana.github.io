df77a353f4d1c8c06d0f70ee5a54f4a34002bfcf
nanayves <yves.g.nana@gmail.com>
2024-10-29 11:52:18 +0100

    Updated profile
    Signed-off-by: nanayves <yves.g.nana@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import NextLink from 'next/link'
+import Image from 'next/image'
 import {
   Link,
   Container,
@@ -7,177 +8,235 @@ import {
   Button,
   List,
   ListItem,
-  chakra
+  useColorModeValue,
 } from '@chakra-ui/react'
+
+// Icons
+import { IoLogoTwitter, IoLogoLinkedin, IoFileTraySharp } from 'react-icons/io5'
+import { MdOutlineMailOutline } from "react-icons/md"
+import { SiGooglescholar } from "react-icons/si"
+
+// Components
 import Paragraph from '../components/paragraph'
 import { BioSection, BioYear } from '../components/bio'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
-import { IoLogoTwitter, IoLogoLinkedin, IoFileTraySharp } from 'react-icons/io5'
-import Image from 'next/image'
-import { mode } from '@chakra-ui/theme-tools'
-import me from '../public/me.jpg'
-import { MdOutlineMailOutline } from "react-icons/md"
-import { SiGooglescholar } from "react-icons/si"
 
-const Home = () => (
-  <Layout>
-    <Container maxW="container.md">
-      
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading 
-          as="h2" 
-          variant="page-title"
-          style={{'letterSpacing': '-0.3px'}}
-          >
-            <h1>Yves Gaetan Nana Teukam</h1>
-          </Heading>
-          <p>Data Scientist</p>
-        </Box>
-        <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
-          textAlign="center"
-        >
+// Assets
+import me from '..//public/me.jpeg'
+
+const Home = () => {
+  const buttonColorScheme = useColorModeValue('teal', 'pink')
+
+  return (
+    <Layout>
+      <Container maxW="container.md">
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading 
+              as="h1" 
+              variant="page-title"
+              style={{ letterSpacing: '-0.3px' }}
+            >
+              Yves Gaetan Nana Teukam
+            </Heading>
+            <p>Pre-Doctoral Research Scientist</p>
+          </Box>
           <Box
-            borderColor="whiteAlpha.800"
-            borderWidth={0}
-            borderStyle="solid"
-            w="200px"
-            h="200px"
-            display="inline-block"
-            borderradius="full"
-            overflow="hidden"
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            textAlign="center"
           >
-            <Image
-              src={me}
-              alt="Profile image"
-              borderradius="full"
-              width="200"
-              height="200"
-            />
+            <Box
+              borderColor="whiteAlpha.800"
+              borderWidth={0}
+              borderStyle="solid"
+              w="200px"
+              h="200px"
+              display="inline-block"
+              borderRadius="full"
+              overflow="hidden"
+            >
+              <Image
+                src={me}
+                alt="Profile image"
+                width={400}
+                height={400}
+                // style={{ borderRadius: '100%' }}
+              />
+            </Box>
           </Box>
         </Box>
-      </Box>
 
-      <Section delay={0.1}>
+        <Section delay={0.1}>
+          <Heading as="h2" variant="section-title">
+            About me
+          </Heading>
+          <Box mb={6}>
+            <Paragraph sx={{ textAlign: 'justify', mb: 4 }}>
+              I am Yves Gaetan Nana Teukam, a Pre-Doctoral Researcher at IBM Research Zürich, focused on developing AI/ML methods to enhance scientific discovery and contribute to open-source projects. Currently, I am pursuing my Ph.D. in Biomedical Engineering at IBM Research and Eindhoven University of Technology, expected to be completed in March 2025.
+            </Paragraph>
+          </Box>
+          
+          <Box mb={6}>
+            <Paragraph sx={{ textAlign: 'justify', mb: 4 }}>
+              My research integrates large language model fine tuning for protein engineering and the development of efficient ML pipelines for biocatalysis. Notable projects include Enzeptional, an end-to-end ML pipeline for enzyme optimization that integrates large language models with evolutionary optimization techniques. I also created LM-ABC, a chatbot framework that automates bioinformatics workflows, and RXNAAMapper, a transformer-based tool that achieved state-of-the-art accuracy in enzymatic binding site prediction.
+              In addition to these projects, I have actively contributed to several open-source initiatives. My involvement with the GT4SD library focuses on training generative models to accelerate scientific discovery. I have also developed molecular dynamics simulation frameworks for validating AI-generated protein designs and implemented techniques for transfer learning in reaction prediction.
+            </Paragraph>
+          </Box>
 
-        <Heading as="h3" variant="section-title">
-          About me
-        </Heading>
-        <Paragraph
-        
-        >
-        Hi! I'm a data scientist based in Zürich. I have 4 years of experience in developing 
-        AI/ML-based tools to address complex biological problems. I'm currently completing a Ph.D. in Biomedical Engineering 
-        with a focus on Language Modelling for Protein Design at <Link as={NextLink} href="https://research.ibm.com/labs/zurich/" target='blank_' passHref scroll={false}> IBM Research Zürich </Link>
-        and <Link as={NextLink} href="https://www.tue.nl/en/" target='blank_' passHref scroll={false}> Eindhoven University of Technology </Link>,
-        expecting to graduate in January 2025. My expertise includes machine learning, generative modelling, NLP, Omics, and bioinformatics. 
-        I have a proven track record of optimizing biomolecules and enhancing model performance for biocatalysis, drug discovery, and green chemistry. 
-        I'm proficient in advanced machine learning frameworks and deep learning libraries, 
-        such as <Link as={NextLink} href="https://www.tensorflow.org/" target='blank_' passHref scroll={false}> TensorFlow </Link>, <Link as={NextLink} href="https://huggingface.co/docs/transformers/index" target='blank_' passHref scroll={false}> Transformers </Link>,
-        and <Link as={NextLink} href="https://pytorch.org/" target='blank_' passHref scroll={false}> PyTorch </Link>,
-        for analyzing and modeling complex biological datasets. I've also contributed to open-source projects like <Link as={NextLink} href="https://github.com/GT4SD" target='blank_' passHref scroll={false}> GT4SD </Link>
-        and published in high-impact journals like  <Link as={NextLink} href="https://www.nature.com/comms/" target='blank_' passHref scroll={false}> Nature Communications </Link>. 
-        I am fluent in English, French, Italian, and Spanish. I am strongly committed to driving innovative projects, fostering collaboration, and achieving team goals.
-        </Paragraph>
-      </Section>
+          <Box mb={6}>
+            <Paragraph sx={{ textAlign: 'justify', mb: 4 }}>
+              Previously, I led a bioinformatics project at StemAway, guiding an international team through gene expression analysis while implementing an automated quality control pipeline that halved analysis time. My internship at Sequentia Biotech provided me with hands-on experience in microbiome analysis and enterotype classification.
+            </Paragraph>
+          </Box>
 
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>01/2022 to 01/2025 (expected graduation)</BioYear>
-          Ph.D. in Biomedical Engineering at IBM Research Zürich & Eindhoven University of Technology – Zürich, Switzerland & Eindhoven, Netherlands
-        </BioSection>
-        <BioSection>
-          <BioYear>09/2019 to 10/2021</BioYear>
-          Master of Science in Data Science at University of Rome La Sapienza – Roma, Italy
-        </BioSection>
-        <BioSection>
-          <BioYear>09/2018 to 02/2019</BioYear>
-          Exchange Program Erasmus at ESCI-Universitad Pompeu Fabra – Barcelona, Spain
-        </BioSection>
-        <BioSection>
-          <BioYear>09/2016 to 06/2019</BioYear>
-          Bachelor of Science in Bioinformatics at University of Rome La Sapienza – Roma, Italy
-        </BioSection>
-      </Section>
+          <Box mb={6}>
+            <Paragraph sx={{ textAlign: 'justify', mb: 4 }}>
+              My research has been published in leading journals such as Nature Communications and the Computational and Structural Biotechnology Journal. I have received recognition for my contributions, including the IEEE Open Software Service Award and the Sandmeyer Award from the Swiss Chemical Society.
+            </Paragraph>
+          </Box>
 
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          Links
-        </Heading>
-        <List spacing={0}>
-          <ListItem>
-            <Link href="mailto:yves.g.nana@gmail.com" target="_blank">
-              <Button
-                variant="ghost"
-                style={{"textAlign": "left !important", "paddingLeft": "0px"}}
-                colorScheme={mode('#319795', '#ff63c3')}
-                leftIcon={<MdOutlineMailOutline />}
-                paddingLeft={0}
-              >
-                Email
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://twitter.com/YvesGNT" target="_blank">
-              <Button
-                variant="ghost"
-                style={{"textAlign": "left !important", "paddingLeft": "0px"}}
-                colorScheme={mode('#319795', '#ff63c3')}
-                leftIcon={<IoLogoTwitter />}
-                paddingLeft={0}
-              >
-                X/Twitter
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://scholar.google.com/citations?user=nvNyJiIAAAAJ&hl=en" target="_blank">
-              <Button
-                variant="ghost"
-                style={{"textAlign": "left !important", "paddingLeft": "0px"}}
-                colorScheme={mode('#319795', '#ff63c3')}
-                leftIcon={<SiGooglescholar />}
-                paddingLeft={0}
-              >
-                Google Scholar
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://www.linkedin.com/in/nana-yves/" target="_blank">
-              <Button
-                variant="ghost"
-                style={{"textAlign": "left !important", "paddingLeft": "0px"}}
-                colorScheme={mode('#319795', '#ff63c3')}
-                leftIcon={<IoLogoLinkedin />}
-              >  Linkedin
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://orcid.org/0000-0002-6954-5119" target="_blank">
-              <Button
-                variant="ghost"
-                style={{"textAlign": "left !important", "paddingLeft": "0px"}}
-                colorScheme={mode('#319795', '#ff63c3')}
-                leftIcon={<IoFileTraySharp />}
-              >  Orcid
-              </Button>
-            </Link>
-          </ListItem>
-        </List>
-      </Section>
-    </Container>
-  </Layout>
-)
+          <Box mb={6}>
+            <Paragraph sx={{ textAlign: 'justify', mb: 4 }}>
+              I hold a Master's degree in Data Science and a Bachelor's degree in Bioinformatics from the University of Rome La Sapienza. This interdisciplinary background empowers me to tackle complex biological challenges through innovative AI solutions.
+            </Paragraph>
+          </Box>
+
+          <Box mb={6}>
+            <Paragraph sx={{ textAlign: 'justify', mb: 4 }}>
+              My expertise spans various domains such as protein design, biocatalysis, drug discovery, and green chemistry, all aimed at making scientific discovery more efficient and accessible through AI technologies.
+            </Paragraph>
+          </Box>
+
+          <Box mb={6}>
+            <Paragraph sx={{ textAlign: 'justify', mb: 4 }}>
+              On this website, you can explore my research projects and publications. If you are interested in discussing applications of AI in scientific discovery or potential collaborations, please feel free to reach out.
+            </Paragraph>
+          </Box>
+        </Section>
+
+        <Section delay={0.2}>
+          <Heading as="h2" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>01/2022 to 03/2025 (expected graduation)</BioYear>
+            Ph.D. in Biomedical Engineering at IBM Research Zürich & Eindhoven University of Technology – Zürich, Switzerland & Eindhoven, Netherlands
+          </BioSection>
+
+          <Box mb={6} /> {/* Add space between education entries */}
+
+          <BioSection>
+            <BioYear>09/2019 to 10/2021</BioYear>
+            Master of Science in Data Science at University of Rome La Sapienza – Roma, Italy
+          </BioSection>
+
+          <Box mb={6} /> {/* Add space between education entries */}
+
+          <BioSection>
+            <BioYear>09/2018 to 02/2019</BioYear>
+            Exchange Program Erasmus at ESCI-Universitad Pompeu Fabra – Barcelona, Spain
+          </BioSection>
+
+          <Box mb={6} /> {/* Add space between education entries */}
+
+          <BioSection>
+            <BioYear>09/2016 to 06/2019</BioYear>
+            Bachelor of Science in Bioinformatics at University of Rome La Sapienza – Roma, Italy
+          </BioSection>
+        </Section>
+
+        <Section delay={0.3}>
+          <Heading as="h2" variant="section-title">
+            Links
+          </Heading>
+          <List spacing={3}> {/* Added spacing between list items */}
+            <ListItem>
+              <Link href="mailto:yves.g.nana@gmail.com" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme={buttonColorScheme}
+                  leftIcon={<MdOutlineMailOutline />}
+                  sx={{ 
+                    textAlign: 'left',
+                    paddingLeft: 0
+                  }}
+                >
+                  Email
+                </Button>
+              </Link>
+            </ListItem>
+
+            <ListItem>
+              <Link href="https://twitter.com/YvesGNT" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme={buttonColorScheme}
+                  leftIcon={<IoLogoTwitter />}
+                  sx={{ 
+                    textAlign: 'left',
+                    paddingLeft: 0
+                  }}
+                >
+                  X/Twitter
+                </Button>
+              </Link>
+            </ListItem>
+
+            <ListItem>
+              <Link href="https://scholar.google.com/citations?user=nvNyJiIAAAAJ&hl=en" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme={buttonColorScheme}
+                  leftIcon={<SiGooglescholar />}
+                  sx={{ 
+                    textAlign: 'left',
+                    paddingLeft: 0
+                  }}
+                >
+                  Google Scholar
+                </Button>
+              </Link>
+            </ListItem>
+
+            <ListItem>
+              <Link href="https://www.linkedin.com/in/nana-yves/" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme={buttonColorScheme}
+                  leftIcon={<IoLogoLinkedin />}
+                  sx={{ 
+                    textAlign: 'left',
+                    paddingLeft: 0
+                  }}
+                >
+                  Linkedin
+                </Button>
+              </Link>
+            </ListItem>
+
+            <ListItem>
+              <Link href="https://orcid.org/0000-0002-6954-5119" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme={buttonColorScheme}
+                  leftIcon={<IoFileTraySharp />}
+                  sx={{ 
+                    textAlign: 'left',
+                    paddingLeft: 0
+                  }}
+                >
+                  Orcid
+                </Button>
+              </Link>
+            </ListItem>
+          </List>
+        </Section>
+      </Container>
+    </Layout>
+  )
+}
 
 export default Home
-// export { getStaticProps } from '../components/chakra'
--- pages/works.js
+++ pages/works.js
@@ -24,7 +24,19 @@ const Works = () => (
             meta2="PAPER"
             meta1="2024"
             >
-              with Loïc Kwate Dassi, Matteo Manica, Daniel Probst, Philippe Schwaller, and Teodoro Laino
+              Teukam, Yves Gaetan Nana, et al. 
+          </LinkGridItem>
+        </Section>
+
+        <Section delay={0.3}>
+          <LinkGridItem
+          href=""
+            title="A language model assistant for biocatalysis"
+            meta_color_2={"green"}
+            meta2="PREPRINT"
+            meta1="2024"
+            >
+            Teukam, Yves Gaetan Nana, et al.
           </LinkGridItem>
         </Section>
 
@@ -36,7 +48,7 @@ const Works = () => (
             meta2="PREPRINT"
             meta1="2024"
             >
-            with Federico Zipoli, Teodoro Laino, Emanuele Criscuolo, Francesca Grisoni, and Matteo Manica
+            Teukam, Yves Gaetan Nana, et al. 
           </LinkGridItem>
         </Section>
 
@@ -48,7 +60,7 @@ const Works = () => (
             meta2="PAPER"
             meta1="2023"
             >
-            with Manica Matteo, Jannis Born, Joris Cadow, Dimitrios Christofidellis, Ashish Dave, Dean Clarke et al.
+            M. Manica, J. Born, J. Cadow, D. Christofidellis, A. Dave, D. Clarke, Teukam, Yves Gaetan Nana, et al.
           </LinkGridItem>
         </Section>
 
@@ -60,7 +72,7 @@ const Works = () => (
             meta2="PAPER"
             meta1="2022"
             >
-            with Probst Daniel, Matteo Manica, Alessandro Castrogiovanni, Federico Paratore, and Teodoro Laino
+            D. Probst, M. Manica, Teukam, Yves Gaetan Nana, A. Castrogiovanni, F. Paratore,  T. Laino
           </LinkGridItem>
         </Section>
 
@@ -113,6 +125,36 @@ const Works = () => (
             meta1="2022"
             >
             GT4SD aims to accelerate scientific discovery by creating an open community around generative models, making them accessible and easier to use for scientists. Their open-source library, gt4sd-core, provides tools for generating hypotheses and fine-tuning AI models, facilitating the adoption of generative AI in scientific research.
+          </LinkGridItem>
+        </Section>
+
+        <Section delay={0.5}>
+          <LinkGridItem
+            href="https://github.com/GT4SD/lm-assistant-for-biocatalysis"
+            title="LM-ABC"
+            meta1="2024"
+            >
+            This project focuses on streamlining and automating key tasks in protein modeling, optimization, and the design of experiments for biocatalyzed reactions.
+          </LinkGridItem>
+        </Section>
+
+        <Section delay={0.5}>
+          <LinkGridItem
+            href="https://github.com/GT4SD/enzeptional"
+            title="Enzeptional"
+            meta1="2024"
+            >
+            This study introduces a framework that combines LLMs with genetic algorithms (GAs) to optimize enzymes.
+          </LinkGridItem>
+        </Section>
+
+        <Section delay={0.5}>
+          <LinkGridItem
+            href="https://github.com/rxn4chemistry/rxnaamapper"
+            title="RXNAAMapper"
+            meta1="2022"
+            >
+            The tool can capture the signal characterizing amino acid (AA) binding sites using linguistic representations for proteins and their molecular substrates, performing unsupervised binding site prediction from protein sequences and reaction SMILES..
           </LinkGridItem>
         </Section>
               
